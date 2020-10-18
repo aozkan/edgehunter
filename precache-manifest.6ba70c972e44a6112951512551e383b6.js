fecfcbf82d4706aa62695291e5559ceafde556f1
aozkan <armagan.ozkan@gmail.com>
2020-10-18 14:27:38 -0400

    Updates

---
+++ precache-manifest.6ba70c972e44a6112951512551e383b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56d3dfeadff6f3f49e4da9b6431b69e8",
+    "url": "/edgehunter/index.html"
+  },
+  {
+    "revision": "a2d0c3afd336ef8b96af",
+    "url": "/edgehunter/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2d4a498be09331e5d6b9",
+    "url": "/edgehunter/static/js/2.731daf65.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/edgehunter/static/js/2.731daf65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d0c3afd336ef8b96af",
+    "url": "/edgehunter/static/js/main.aa359e87.chunk.js"
+  },
+  {
+    "revision": "8b820f57116d51e9f25a",
+    "url": "/edgehunter/static/js/runtime-main.f353f432.js"
+  }
+]);
